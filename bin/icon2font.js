c84bb0db3edf28a76d81ee6c01a1171c7bdcae1d
yangfang.0285 <yangfang.0285@bytedance.com>
2018-11-12 11:30:47 +0800

    fix problem

--- bin/icon2font.js
+++ bin/icon2font.js
@@ -6,7 +6,7 @@ const inquirer = require('inquirer');
 const chalk = require('chalk');
 const _ = require('lodash')
 program
-    .version('2.1.1')
+    .version('2.2.0')
     .usage('<inputdir> <outputdir> <fontname>')
     .description('convert icon to font automaticly')
     .option('-i, --inputdir <inputdir>', 'inputdir')
